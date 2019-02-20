d08ca32f51fbb93485135e6dde35ff61fd1f557d
yousafi <40589224+yousafi@users.noreply.github.com>
2019-02-20 09:44:05 +0430

    Vue JS Shopping cart

---
+++ src/App.vue
@@ -0,0 +1,100 @@
+<template>
+
+<div class="container">
+
+  <div class="row">
+
+    <div class="col-md-7 mt-20">
+
+      <div class="row">
+
+
+
+      <div class="col-md-6" v-for="product in products" :key="product.id">
+
+          <Product :isInCart="isInCart(product)" v-on:add-to-cart="addToCart(product)" :product="product"></Product>
+        
+
+      </div>
+
+
+
+      </div>
+
+      
+
+    </div>
+
+    <div class="col-md-5 my-5">
+      <Cart v-on:pay="makeCartEmpty()" v-on:remove-from-cart="removeFromCart($event)"  :items="cart"></Cart>
+    </div>
+
+
+
+  </div>
+
+</div>
+  
+</template>
+
+<script>
+
+import products from '@/products.json'
+import Product from '@/components/Product.vue'
+import Cart from '@/components/Cart.vue'
+
+export default {
+
+  name:'app',
+
+  components: {
+
+      Product,
+      Cart
+    },
+
+  data() {
+
+      return {
+
+        products,
+        cart: []    
+      }
+
+      
+  },
+
+  methods: {
+
+        addToCart(product) {
+          this.cart.push(product)
+        },
+        
+        isInCart(product) {
+          const item = this.cart.find(item => item.id === product.id)
+          if(item){
+            return true;
+          }
+          return false;
+        },
+        removeFromCart(product) {
+          this.cart = this.cart.filter(item => item.id != product.id)
+        },
+        makeCartEmpty() {
+          this.cart = []
+          alert('Shopping Completed')
+        }
+
+      }
+
+ 
+}
+</script>
+
+<style>
+
+  body {
+    background-color: #f8f8f3;
+  }
+
+</style>
---
+++ src/components/Cart.vue
@@ -0,0 +1,33 @@
+<template>
+    <ul class="list-group">
+
+        <li class="list-group-item" v-for="item in items" :key="item.id">
+
+                {{item.title}} - $ {{item.price}}
+
+                <button @click="$emit('remove-from-cart', item)" class="btn badge badge-danger float-right">Remove from the Cart</button>
+
+        </li>
+
+        <div v-if="items.length > 0" class="card">
+            <h4 class="text-center">Total $ {{ total }}</h4>
+        </div>
+
+        <button @click="$emit('pay')" v-if="items.length > 0" :disabled="items.length === 0" class="btn btn-info my-3">Pay Now</button>
+
+    </ul>
+</template>
+
+<script>
+export default {
+    props: ['items'],
+
+    computed: {
+        total() {
+
+            return this.items.reduce((acc, item) => acc + Number(item.price), 0);
+        }
+    }
+}
+</script>
+
